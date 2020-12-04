f23ae4d52ccf0e1b34ed33fc884089ce70d796fa
Samuel Rioja <43000324+SamuelRioTz@users.noreply.github.com>
2020-12-04 03:15:51 -0400

    build script frontend

---
+++ build.sh
@@ -0,0 +1,5 @@
+ #!/bin/bash
+npm run copy-fonts &&
+npm run build &&
+rm -r /Users/sam/projects/groots/kobo-docker/.vols/static/kpi/compiled &&
+mv /Users/sam/projects/groots/kpi/jsapp/compiled /Users/sam/projects/groots/kobo-docker/.vols/static/kpi
