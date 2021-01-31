5cd5d70890442611c6985ebe11dbb9f18d579576
Samuel Rioja <samuelriotz@hotmail.com>
2021-01-31 13:34:04 -0400

    adding new rebuild script

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
  #!/bin/bash
-npm i &&
-npm run copy-fonts &&
-npm run build &&
-rm -r /Users/sam/projects/groots/kobo-docker/.vols/static/kpi/compiled &&
-mv /Users/sam/projects/groots/kpi/jsapp/compiled /Users/sam/projects/groots/kobo-docker/.vols/static/kpi
+rm -rf "./jsapp/fonts" &&
+rm -rf "./jsapp/compiled" &&
+npm run copy-fonts && npm run build &&
+python manage.py collectstatic --noinput &&
+git submodule init &&
+git submodule update --remote &&
+python manage.py compilemessages
